00b7a4599ea5d94db4a7501411a4b823fd7b9541
Arsh Goel <arshgoel16@gmail.com>
2025-03-30 03:47:51 +0530

    first commit

--- build_files.sh
+++ build_files.sh
@@ -8,8 +8,12 @@ python -m pip install --no-cache-dir opencv-python face_recognition dlib opencv-
 python -m pip install -r requirements.txt
 
 # Run migrations
-python manage.py makemigrations
+python manage.py makemigrations 
 python manage.py migrate
+python manage.py makemigrations Accounts
+python manage.py migrate Accounts
+python manage.py makemigrations Services
+python manage.py migrate Services
 
 # Collect static files
 python manage.py collectstatic --noinput --clear
