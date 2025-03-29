bd67dfc8fff2dbe6a3d87ca1a151b736ee39663b
Arsh Goel <arshgoel16@gmail.com>
2025-03-30 03:36:29 +0530

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,17 @@
 echo "BUILD START"
-python -m pip install https://github.com/sachadee/Dlib/releases/download/v19.22.99/dlib-19.22.99-cp310-cp310-win_amd64.whl
-python -m pip install opencv-python face_recognition
+
+# Install dependencies
+python -m pip install --upgrade pip
+python -m pip install --no-cache-dir opencv-python face_recognition dlib
+
+# Install additional dependencies from requirements.txt
 python -m pip install -r requirements.txt
-python -m manage.py makemigrations 
-python -m manage.py migrate
-python -m manage.py makemigrations Accounts
-python -m manage.py migrate Accounts
-python -m manage.py makemigrations Services
-python -m manage.py migrate Services
+
+# Run migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# Collect static files
 python manage.py collectstatic --noinput --clear
+
 echo "BUILD END"
