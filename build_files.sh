d7b47e205bee141fd90ade9f08accc62498d109a
Arsh Goel <arshgoel16@gmail.com>
2025-03-30 20:58:04 +0530

    first commit

--- build_files.sh
+++ build_files.sh
@@ -2,11 +2,11 @@ echo "BUILD START"
 
 # Install dependencies
 python -m pip install --upgrade pip
-python -m pip install --no-cache-dir django opencv-python cmake face-recognition 
+python -m pip install --no-cache-dir django whitenoise opencv-python cmake face-recognition 
 
 
 # Install additional dependencies from requirements.txt
-python -m pip install -r requirements.txt
+# python -m pip install -r requirements.txt
 
 # Run migrations
 python manage.py makemigrations 
