7b37ad48c0fd4a5cd9235a649326ea6def7dde78
Arsh Goel <arshgoel16@gmail.com>
2025-03-30 21:27:10 +0530

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "BUILD START"
-# python3 -m pip install -r requirements.txt
-python3 -m pip install django opencv-python whitenoise cmake face-recognition
+python3 -m pip install -r requirements.txt
+# python3 -m pip install django opencv-python whitenoise cmake face-recognition
+
 python3 manage.py collectstatic
 echo "BUILD END"
