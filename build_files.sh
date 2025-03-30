e1a24e21a8170e827f1362b88705128a4fbdd9d6
Arsh Goel <arshgoel16@gmail.com>
2025-03-30 21:53:18 +0530

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,22 @@
 echo "BUILD START"
 
-# Install dependencies
-# python -m pip install --upgrade pip
-python3 -m /Django_Final/wsgi.py
+# Update package lists
+apt-get update
+
+# Install dependencies for dlib
+apt-get install -y cmake g++ make python3-dev
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Manually install dlib before other packages
+python3 -m pip install cmake
+python3 -m pip install dlib
+
+# Install other Python dependencies
+python3 -m pip install django opencv-python whitenoise face-recognition
+
 # Collect static files
-python3 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput
 
 echo "BUILD END"
