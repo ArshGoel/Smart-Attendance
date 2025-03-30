c79811c2e562d8b53fa0bcdc1e10df063f744c04
Arsh Goel <arshgoel16@gmail.com>
2025-03-30 21:11:25 +0530

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,22 +1,2 @@
-echo "BUILD START"
-
-# Install dependencies
-# python -m pip install --upgrade pi
-pip install django whitenoise opencv-python cmake face-recognition 
-
-
-# Install additional dependencies from requirements.txt
-# python -m pip install -r requirements.txt
-
-# Run migrations
-python manage.py makemigrations 
-python manage.py migrate
-python manage.py makemigrations Accounts
-python manage.py migrate Accounts
-python manage.py makemigrations Services
-python manage.py migrate Services
-
-# Collect static files
-python manage.py collectstatic --noinput --clear
-
-echo "BUILD END"
+pip install -r requirements.txt
+python3.10.10 manage.py collectstatic
