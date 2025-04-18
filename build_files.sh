c564015a06458a6d085758b82c34247c9901a110
Arsh Goel <arshgoel16@gmail.com>
2025-04-18 22:58:47 +0530

    try

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "BUILD START"
 python3 -m pip install -r requirements.txt
+pip install psycopg2-binary
 python3 manage.py makemigrations  
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput --clear
