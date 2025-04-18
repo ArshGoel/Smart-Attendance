bbf6e50d2d4c9fad0b4bcc69d8a4a223b63ce268
Arsh Goel <arshgoel16@gmail.com>
2025-04-18 23:01:22 +0530

    try

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,11 @@
 echo "BUILD START"
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade setuptools
+python3 -m pip install --upgrade wheel
+python3 -m pip install --upgrade pipenv
+python3 -m pip install --upgrade django
+python3 -m pip install psycopg2-binary
 python3 -m pip install -r requirements.txt
-pip install psycopg2-binary
 python3 manage.py makemigrations  
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput --clear
