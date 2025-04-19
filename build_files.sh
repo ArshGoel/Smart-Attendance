04b1f37983d9a41d423ba253ba43c804f9badf67
Arsh Goel <arshgoel16@gmail.com>
2025-04-19 09:54:07 +0530

    try

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3 -m pip install static/dlib_library/dlib-19.22.99-cp310-cp310-win_amd64.whl.whl
+python3 -m pip install static/dlib_library/dlib-19.22.99-cp310-cp310-win_amd64.whl
 python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations  
 python3 manage.py migrate
