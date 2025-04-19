5f3019e764b10cb0b515c9346aaf5a570adc10b5
Arsh Goel <arshgoel16@gmail.com>
2025-04-19 10:18:52 +0530

    dlib

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,6 @@
 echo "BUILD START"
-# Update and install dependencies (for Debian/Ubuntu-based)
-apk update && apk add build-base cmake python3-dev boost-dev
 
-# Upgrade pip
-python3 -m pip install --upgrade pip
-
-# Install dlib from source
-python3 -m pip install dlib
+python3 -m pip install cmake
 python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations  
 python3 manage.py migrate
