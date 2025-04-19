3f162e0849277545468d4ee8353a2f01120d7fd5
Arsh Goel <arshgoel16@gmail.com>
2025-04-19 10:12:26 +0530

    dlib

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,12 @@
 echo "BUILD START"
-python3 -m pip install './static/dlib_library/dlib-19.24.99-cp312-cp312-win_amd64.whl'
-python3 -m pip install https://github.com/z-mahmud22/Dlib_Windows_Python3.x/raw/main/dlib-19.24.99-cp312-cp312-win_amd64.whl
+# Update and install dependencies (for Debian/Ubuntu-based)
+apt-get update && apt-get install -y build-essential cmake python3-dev libboost-all-dev
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Install dlib from source
+python3 -m pip install dlib
 python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations  
 python3 manage.py migrate
